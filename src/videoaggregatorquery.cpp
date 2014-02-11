4a194cfee3fba8a7ec8b331b587e2eefb379667c
Jussi Pakkanen <jussi.pakkanen@canonical.com>
2014-02-11 11:41:42 +0200

    Created video aggregator scope skeleton.

---
+++ src/videoaggregatorquery.cpp
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2014 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by Jussi Pakkanen <jussi.pakkanen@canonical.com>
+ *
+ */
+
+#include "videoaggregatorquery.h"
+#include "resultforwarder.h"
+#include <unity/scopes/Annotation.h>
+#include <unity/scopes/CategorisedResult.h>
+#include <unity/scopes/CategoryRenderer.h>
+#include <unity/scopes/Category.h>
+#include <unity/scopes/Query.h>
+#include <unity/scopes/SearchReply.h>
+
+#include <unity/scopes/SearchListener.h>
+
+using namespace unity::scopes;
+
+VideoAggregatorQuery::VideoAggregatorQuery(Query const& query, ScopeProxy local_scope, ScopeProxy online_scope) :
+query(query), local_scope(local_scope), online_scope(online_scope) {
+}
+
+VideoAggregatorQuery::~VideoAggregatorQuery() {
+}
+
+void VideoAggregatorQuery::cancelled() {
+}
+
+void VideoAggregatorQuery::run(unity::scopes::SearchReplyProxy const& parent_reply) {
+    SearchListener::SPtr local_reply(new ResultForwarder(parent_reply));
+    create_subquery(local_scope, query.query_string(), local_reply);
+    SearchListener::SPtr online_reply(new ResultForwarder(parent_reply));
+    create_subquery(online_scope, query.query_string(), online_reply);
+}
---
+++ src/videoaggregatorscope.cpp
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2014 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by Jussi Pakkanen <jussi.pakkanen@canonical.com>
+ *
+ */
+
+#include "videoaggregatorscope.h"
+#include "videoaggregatorquery.h"
+#include <unity/scopes/Registry.h>
+#include <unity/scopes/Category.h>
+#include <unity/scopes/CategoryRenderer.h>
+
+using namespace unity::scopes;
+
+const char *LOCALSCOPE = "mediascanner-video";
+const char *ONLINESCOPE = "scope-onlinevideo";
+
+int VideoAggregatorScope::start(std::string const&, unity::scopes::RegistryProxy const& registry) {
+    CategoryRenderer basic;
+    local_scope = registry->get_metadata(LOCALSCOPE).proxy();
+    // Disable online scopes until the smart scope server proxy is working.
+    online_scope = registry->get_metadata(ONLINESCOPE).proxy();
+    return VERSION;
+}
+
+void VideoAggregatorScope::stop() {
+}
+
+QueryBase::UPtr VideoAggregatorScope::create_query(Query const& q,
+                                                   SearchMetadata const&) {
+    QueryBase::UPtr query(new VideoAggregatorQuery(q, local_scope, online_scope));
+    return query;
+}
+
+QueryBase::UPtr VideoAggregatorScope::preview(Result const& /*result*/, ActionMetadata const& /*hints*/) {
+    return nullptr;
+}
+
+#define EXPORT __attribute__ ((visibility ("default")))
+
+extern "C"
+{
+
+    EXPORT
+    unity::scopes::ScopeBase*
+    // cppcheck-suppress unusedFunction
+    UNITY_SCOPE_CREATE_FUNCTION()
+    {
+        return new VideoAggregatorScope();
+    }
+
+    EXPORT
+    void
+    // cppcheck-suppress unusedFunction
+    UNITY_SCOPE_DESTROY_FUNCTION(unity::scopes::ScopeBase* scope_base)
+    {
+        delete scope_base;
+    }
+
+}
