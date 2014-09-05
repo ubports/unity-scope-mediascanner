4e4e80066008b4deba8cd54147f900eca1900c0a
Pawel Stolowski <pawel.stolowski@canonical.com>
2014-09-05 11:53:56 +0200

    Enable video:/// uris back.

--- src/video-scope.cpp
+++ src/video-scope.cpp
@@ -187,12 +187,9 @@ void VideoQuery::run(SearchReplyProxy const&reply) {
         }
 
         std::string uri = media.getUri();
-        /* FIXME: reenable once mediaplayer-app is fixed:
-             https://bugs.launchpad.net/mediaplayer-app/+bug/1340952
         if (uri.find("file://") == 0) {
             uri = "video://" + uri.substr(7); // replace file:// with video://
         }
-        */
 
         CategorisedResult res(cat);
         res.set_uri(uri);
